92a0a9f2cead8ee803efa39258638389f2924d52
Nada Amin <namin@seas.harvard.edu>
2021-12-18 21:38:24 +0100

    move to component

---
+++ src/Feelings.svelte
@@ -0,0 +1,90 @@
+<script>
+  import feelings from './feelings.ts';
+  import { onMount } from 'svelte';
+  import * as d3 from 'd3';
+
+  let width = 900;
+  let height = 900;
+  let bandSize = 150;
+  let radius = Math.min(width, height) / 2;
+
+  /**
+   * Calculate the correct distance to rotate each label based on its location in the sunburst.
+   * @param {Node} d
+   * @return {Number}
+   */
+  function computeTextRotation(d) {
+    var angle = (d.x0 + d.x1) / Math.PI * 90;
+    return (angle < 180) ? angle - 90 : angle + 90;
+  }
+
+  onMount(() => {
+    // Size our <svg> element, add a <g> element, and move translate 0,0 to the center of the element.
+    var g = d3.select('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+    // Create our sunburst data structure and size it.
+    var partition = d3.partition()
+        .size([2 * Math.PI, radius]);
+
+    // Find the root node of our data, and begin sizing process.
+    var root = d3.hierarchy(feelings)
+        .sum(function (d) { return d.size});
+
+    // Calculate the sizes of each arc that we'll draw later.
+    partition(root);
+    var arc = d3.arc()
+        .startAngle(function (d) { return d.x0 })
+        .endAngle(function (d) { return d.x1})
+        .innerRadius(function (d) { return d.depth == 0 ? 0 : (d.depth -1) * bandSize })
+        .outerRadius(function (d) { return d.depth == 0 ? 0 : d.depth * bandSize });
+
+    // Add a <g> element for each node in thd data, then append <path> elements and draw lines based on the arc
+    // variable calculations. Last, color the lines and the slices.
+    g.selectAll('g')
+      .data(root.descendants())
+      .enter().append('g').attr("class", "node").append('path')
+      .attr("display", function (d) { return d.depth ? null : "none"; })
+      .attr("d", arc)
+      .style("stroke", "black")
+      .style("fill", function (d) { return d.data.color; })
+      .attr("id", function (d) { return d.data.id; })
+      .each(function (d) {
+        var id = d.data.id;
+        var selNode = this.parentNode;
+        selNode.addEventListener('click', function(e) {
+          selNode.classList.toggle('selected');
+        });
+      });
+
+    // Populate the <text> elements with our data-driven titles.
+    g.selectAll(".node")
+      .append("text")
+      .attr("transform", function(d) {
+        return "translate(" + arc.centroid(d) + ")rotate(" + computeTextRotation(d) + ")"; })
+      .attr("dy", ".5em") // rotation align
+      .text(function(d) { return d.parent ? d.data.name : "" })
+      .style("text-anchor", "middle")
+      .attr("fill", function (d) { return d.data.text; });
+
+    g.attr("transform", "rotate(45)")
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+  });
+</script>
+
+<svg></svg>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap');
+
+body {
+  font-family: "Raleway", "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+:global(.selected) {
+  font-weight: bold;
+}
+</style>
