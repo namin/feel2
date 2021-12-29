049836912dd7b08b406d6608f4353fc3a85e693c
Nada Amin <namin@seas.harvard.edu>
2021-12-29 19:12:18 +0100

    now, no google

---
+++ src/routes/now.svelte
@@ -0,0 +1,11 @@
+<script>
+  import App from '$lib/App.svelte';
+  import Feelings, { handlePublicLineClick } from '$lib/Feelings.svelte';
+</script>
+
+<App link='/'>
+  <div>
+      <button on:click={handlePublicLineClick}>Public Line</button>
+  </div>
+  <Feelings/>
+</App>
